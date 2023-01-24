ee421f800b70cc3e51501a0ad4204269b41cfdb0
Roman Annenkov <roman.annenkov@gmail.com>
2023-01-24 12:34:28 +0300

    Task49 solved

---
+++ Task49/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задача 49: Задайте двумерный массив. Найдите элементы, у
+которых оба индекса чётные, и замените эти элементы на их
+квадраты.
+Например, изначально массив
+выглядел вот так:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+*/
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2d);
+Console.WriteLine();
+SquareEvenIndex(array2d);
+PrintMatrix(array2d);
+
+void SquareEvenIndex(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) matrix[i,j] *= matrix[i,j];
+        }
+    }
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] newMatrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            newMatrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return newMatrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
