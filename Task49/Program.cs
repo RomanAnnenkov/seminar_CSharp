88c38cf84fb11dab9485cb275c4be21dad01b684
Roman Annenkov <roman.annenkov@gmail.com>
2023-01-24 13:01:59 +0300

    Task49 optimized

--- Task49/Program.cs
+++ Task49/Program.cs
@@ -16,11 +16,11 @@ PrintMatrix(array2d);
 
 void SquareEvenIndex(int[,] matrix)
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i = i + 2)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j = j + 2)
         {
-            if (i % 2 == 0 && j % 2 == 0) matrix[i,j] *= matrix[i,j];
+            matrix[i,j] *= matrix[i,j];
         }
     }
 }
