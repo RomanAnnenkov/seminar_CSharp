950cc9063161942bd6b3da0a99d271894a726693
Roman Annenkov <roman.annenkov@gmail.com>
2023-01-26 20:28:31 +0300

    Task53 solved

---
+++ Task53/Program.cs
@@ -0,0 +1,47 @@
+﻿
+/* Задача 53: Задайте двумерный массив. Напишите программу,
+которая поменяет местами первую и последнюю строку
+массива. */
+
+int[,] array2d = CreateMatrixRndInt(3, 4, -5, 40);
+PrintMatrix(array2d);
+Console.WriteLine();
+ReplaceFirsLastRow(array2d);
+PrintMatrix(array2d);
+
+void ReplaceFirsLastRow(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        int tmp = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i] = tmp;
+
+    }
+}
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] newMatrix = new int[rows, columns];
+    var rnd = new Random();
+    for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            newMatrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return newMatrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine();
+    }
+}
